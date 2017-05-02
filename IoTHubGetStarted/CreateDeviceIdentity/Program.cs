7e29d8095010d96e5097fb7e242d82d50dd675f2
Mario Tristan <mariotristan@gmail.com>
2017-05-02 19:40:58 -0400

    first version

---
+++ IoTHubGetStarted/CreateDeviceIdentity/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace CreateDeviceIdentity
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+        static string connectionString = "HostName=iothubmt.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=p5FBb7STIfm9Hn8IbsjU2EP2tlWyGdy6sQxwc3gZaHU=";
+        static void Main(string[] args)
+        {
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            AddDeviceAsync().Wait();
+            Console.ReadLine();
+        }
+        private static async Task AddDeviceAsync()
+        {
+            string deviceId = "myFirstDevice";
+            Device device;
+            try
+            {
+                device = await registryManager.AddDeviceAsync(new Device(deviceId));
+            }
+            catch (DeviceAlreadyExistsException)
+            {
+                device = await registryManager.GetDeviceAsync(deviceId);
+            }
+            Console.WriteLine("Generated device key: {0}", device.Authentication.SymmetricKey.PrimaryKey);
+        }
+    }
+}
---
+++ IoTHubGetStarted/SendCloudToDevice/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+
+namespace SendCloudToDevice
+{
+    class Program
+    {
+        static ServiceClient serviceClient;
+        static string connectionString = "HostName=marsiotnyc.azure-devices.net;SharedAccessKeyName=coffeeclient;SharedAccessKey=SRFKGwGTdEJ8cm55akdz/QPqZH2b874zhOrMSNDY7js=";
+
+        static void Main(string[] args)
+        {
+            
+            string message = "{\"Command\":\"Brew\",\"Team\":\"team09\",\"Parameters\":\"Black\"}";
+            Console.WriteLine(message);
+            serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+            ReceiveFeedbackAsync();
+            Console.WriteLine("Press any key to send a C2D message.");
+            Console.ReadLine();
+            SendCloudToDeviceMessageAsync().Wait();
+            Console.ReadLine();
+
+        }
+
+        private async static Task SendCloudToDeviceMessageAsync()
+        {
+            var commandMessage = new Message(Encoding.ASCII.GetBytes("Cloud to device message."));
+            commandMessage.Ack = DeliveryAcknowledgement.Full;
+            await serviceClient.SendAsync("coffeepot", commandMessage);
+        }
+
+        private async static void ReceiveFeedbackAsync()
+        {
+            var feedbackReceiver = serviceClient.GetFeedbackReceiver();
+
+            Console.WriteLine("\nReceiving c2d feedback from service");
+            while (true)
+            {
+                var feedbackBatch = await feedbackReceiver.ReceiveAsync();
+                if (feedbackBatch == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received feedback: {0}", string.Join(", ", feedbackBatch.Records.Select(f => f.StatusCode)));
+                Console.ResetColor();
+
+                await feedbackReceiver.CompleteAsync(feedbackBatch);
+            }
+        }
+
+
+
+    }
+}
